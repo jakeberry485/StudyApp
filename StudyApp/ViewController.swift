f3270d61824560d431e03a82d07e3297de6f9ea7
jakeberry485 <jakeberry1557@gmail.com>
2019-09-25 12:05:24 -0400

    Initial Commit

---
+++ StudyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyApp
+//
+//  Created by  on 9/25/19.
+//  Copyright © 2019 jakesapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
