849ce961b598b7869d54b18ed71c550f991215ed
jakeberry485 <jakeberry1557@gmail.com>
2019-09-27 12:03:53 -0400

    Set up storyboard and actions

--- StudyApp/ViewController.swift
+++ StudyApp/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func Button1(_ sender: Any)
+    {
+
+    }
+    
+    @IBAction func Button2(_ sender: Any)
+    {
+//        Button2.text = 8
+    }
+
+    @IBAction func Button3(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func Button4(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func Button5(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func Button6(_ sender: Any)
+    {
+        
+    }
+    
+
+
+
+
 
 }
+
 
